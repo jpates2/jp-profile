7ab887754f6d7d28ed9db5170be2278c31fdcabb
Jess Pates <jesspates@hotmail.co.uk>
2023-06-17 22:34:45 +0200

    javascript smooth scroll

--- app/javascript/packs/script.js
+++ app/javascript/packs/script.js
@@ -1,3 +1,14 @@
+// window.onbeforeunload = function () {
+//   document.body.scrollTo(0, 0);
+//   console.log("Current scroll X/Y", scrollX, scrollY);
+
+// }
+
+addEventListener("load", (event) => {
+  window.scrollTo(0, 0)
+  console.log("Reload");
+});
+
 const bannerHeader = document.querySelector(".banner-header");
 
 const welcomeText = function () {
@@ -17,3 +28,37 @@ const welcomeText = function () {
 }
 
 welcomeText();
+
+
+const btnScrollToHome = document.querySelector("#home");
+const btnScrollToProject = document.querySelector("#projects");
+const btnScrollToAbout = document.querySelector("#about");
+const btnScrollToContact = document.querySelector("#contact");
+const homeSection = document.querySelector(".home-section");
+const projectSection = document.querySelector(".project-section");
+const aboutSection = document.querySelector(".about-section");
+const contactSection = document.querySelector(".contact-section");
+
+btnScrollToProject.addEventListener("click", function(e) {
+  projectSection.scrollIntoView({
+    behavior: "smooth"
+  })
+})
+
+btnScrollToAbout.addEventListener("click", function(e) {
+  aboutSection.scrollIntoView({
+    behavior: "smooth"
+  })
+})
+
+btnScrollToHome.addEventListener("click", function(e) {
+  homeSection.scrollIntoView({
+    behavior: "smooth"
+  })
+})
+
+btnScrollToContact.addEventListener("click", function(e) {
+  contactSection.scrollIntoView({
+    behavior: "smooth"
+  })
+})
