eed3182bc10214d5b4d62d323ebedbdc24b05ade
Jess Pates <jesspates@hotmail.co.uk>
2023-08-22 23:01:37 +0100

    certs media queries + js event handlers for small screens

--- app/javascript/packs/certs.js
+++ app/javascript/packs/certs.js
@@ -2,6 +2,7 @@ const certsCaret = document.querySelectorAll(".cert-caret");
 const certsLeft = document.querySelectorAll(".cert-left");
 const certsRight = document.querySelectorAll(".cert-right");
 const certsBadge = document.querySelectorAll(".cert-badge");
+const certsImg = document.querySelectorAll(".cert-img");
 const backdrop = document.querySelector(".backdrop");
 
 // Collapsible content
@@ -63,37 +64,36 @@ certsRight.forEach(function(cert) {
 })
 
 
-// Show cert on hover
+// Show cert on hover / click
 
 if (window.innerWidth > 1000) {
   for (let i = 0; i < certsBadge.length; i++) {
     certsBadge[i].addEventListener("mouseover", function() {
+      backdrop.classList.remove("hidden");
       certsBadge[i].closest(".cert-details").querySelector(".cert-img").classList.remove("hidden");
     })
   };
 
   for (let i = 0; i < certsBadge.length; i++) {
     certsBadge[i].addEventListener("mouseout", function() {
+      backdrop.classList.add("hidden");
       certsBadge[i].closest(".cert-details").querySelector(".cert-img").classList.add("hidden");
-
     })
   };
 }
 
-if (window.innerWidth > 600) {
+if (window.innerWidth < 1000) {
   for (let i = 0; i < certsBadge.length; i++) {
     certsBadge[i].addEventListener("click", function() {
+      backdrop.classList.remove("hidden");
       certsBadge[i].closest(".cert-details").querySelector(".cert-img").classList.remove("hidden");
     })
   };
 
   backdrop.addEventListener("click", function() {
-    certsBadge[i].closest(".cert-details").querySelector(".cert-img").classList.add("hidden");
+    backdrop.classList.add("hidden");
+    for (let i = 0; i < certsBadge.length; i++) {
+      certsBadge[i].closest(".cert-details").querySelector(".cert-img").classList.add("hidden");
+    };
   })
-
-  // for (let i = 0; i < certsBadge.length; i++) {
-  //   certsBadge[i].addEventListener("mouseout", function() {
-
-  //   })
-  // };
 }
